d82d018db665137580533c2163ee9499b0dd172d
MMunier <marc@geomaps.de>
2022-03-29 19:41:03 +0000

    initial commit

---
+++ service_hidden/bambi-notes.c
@@ -0,0 +1,363 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <dirent.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#define NL "\n"
+#define STORAGE_DIR "./data/%s/%s"
+
+#define NOTE_SIZE 40
+#define DEFAULT_NOTE "Well, it's a note-taking service. What did you expect?"
+
+
+void setup() {
+    setbuf(stdin, NULL);
+    setbuf(stdout, NULL);
+    setbuf(stderr, NULL);
+    alarm(30);
+}
+
+long getlong() {
+    char buf[40];
+    fgets(buf, sizeof(buf), stdin);
+    return strtol(buf, NULL, 0);
+}
+
+struct User {
+    char username[40];
+    char * notes[10];
+    char in_use[10];
+};
+
+struct User* init_user() {
+    char * base_note = calloc(1, sizeof(DEFAULT_NOTE));
+    strcpy(base_note, DEFAULT_NOTE);
+
+    struct User *user_ptr = calloc(1, sizeof(struct User));
+    user_ptr->notes[0] = base_note;
+    user_ptr->in_use[0] = 1;
+}
+
+void delete_user(struct User *user) {
+    for (int note_idx = 10; note_idx >= 0; note_idx-- ) {
+        if (user->in_use[note_idx]) {
+            free(user->notes[note_idx]);
+        }
+    }
+    free(user);
+}
+
+struct User* user_register() {
+    
+    printf(
+        "Username:\n> "
+    );
+    char username[40];
+    fgets(username, 40, stdin);
+    
+    char path_buf[sizeof(username) + sizeof(STORAGE_DIR) + 0x20];
+    snprintf(path_buf, sizeof(path_buf), STORAGE_DIR, username, "passwd");
+    int access_result = access(path_buf, F_OK);
+    
+    if ( access_result != -1 ) {
+        printf("Username already taken!\n");
+        return 0;
+    }
+      
+    printf(
+        "Password:\n> "
+    );
+    char password[40];
+    fgets(password, 40, stdin);
+    
+    snprintf(path_buf, sizeof(path_buf), STORAGE_DIR, username, "");
+    long res = mkdir(path_buf, 0775);
+    if (res) {
+        perror("Failed to create user directory!");
+        exit(EXIT_FAILURE);
+    }
+
+    snprintf(path_buf, sizeof(path_buf), STORAGE_DIR, username, "passwd");
+    long fd = open(path_buf, O_WRONLY|O_CREAT, 0644);
+    if (fd < 0) { 
+        perror("Failed to create passwd file!");
+        exit(EXIT_FAILURE);
+    }
+
+    write(fd, password, strlen(password));
+    close(fd);
+
+    // Successful login
+    struct User *user = init_user();
+    strcpy(user->username, username);
+    return user;
+}
+
+struct User* user_login() {
+
+    printf(
+        "Username:\n> "
+    );
+    char username[40];
+    fgets(username, 40, stdin);
+    
+    char path_buf[sizeof(username) + sizeof(STORAGE_DIR) + 0x20];
+    snprintf(path_buf, sizeof(path_buf), STORAGE_DIR, username, "passwd");
+    int access_result = access(path_buf, F_OK | R_OK);
+    
+    if ( access_result < 0 ) {
+        printf("User %s does not exist!", username);
+        return 0;
+    }
+    
+    char password_buf[40];
+    long password_fd = open(path_buf, O_RDONLY);
+    if (password_fd < 0) {
+        return (struct User*) password_fd;
+    }
+    
+    int bytes_read = read(password_fd, password_buf, sizeof(password_buf) -1 );
+    close(password_fd);
+
+    password_buf[bytes_read] = 0;
+
+    printf(
+        "Password:\n> "
+    );
+    char user_password[40];
+    fgets(user_password, 40, stdin);
+        
+    if (strcmp(password_buf, user_password)) {
+        puts("Wrong password!");
+        return 0;
+    }
+
+    // Successful login
+    struct User *user = init_user();
+    strcpy(user->username, username);
+    return user;
+}
+
+void main_menu() {
+    printf(
+        "===== [Unauthenticated] =====" NL
+        "   1. Register" NL 
+        "   2. Login" NL
+        "> " 
+    );
+}
+
+void authenticated_menu(struct User *user) {
+    printf(
+        "===== [%s] =====" NL
+        "   1. Create" NL 
+        "   2. Print" NL
+        "   3. List Saved" NL
+        "   4. Delete" NL
+        "   5. Load" NL
+        "   6. Save" NL
+        "> ",
+        user->username
+    );
+}
+
+void create_note(struct User* user) {
+    printf(
+        "Which slot to save the note into?" NL
+        "> "
+    );
+    long idx = getlong();
+    if (user->in_use[idx]) {
+        puts("Already Occupied!");
+    }
+
+    
+
+}
+void print_note(struct User* user) {}
+void list_saved_notes(struct User* user) {
+
+    printf("\n\n===== [%s's Notes] =====\n", user->username);
+
+    char table_header = 0;
+    for (int note_idx = 0; note_idx < 10; note_idx++) {
+        if (user->in_use[note_idx]) {
+            if (!table_header) {
+                printf("Currently Loaded:\n");
+                table_header = 1;
+            }
+            printf("    %d | %s", note_idx, user->notes[note_idx]);
+        }
+    }
+
+
+    // Dunno impl shell injection here?
+    DIR* dirfd = opendir("");
+    if (dirfd < 0) {
+        perror("Failed to open user directory");
+        exit(EXIT_FAILURE);
+    }
+
+    table_header = 0;
+    struct dirent* entry;
+    while ((entry = readdir(dirfd)) != NULL) {
+        if (strcmp(entry->d_name, "passwd") == 0) {
+            continue;
+        }
+
+        if (!table_header) {
+            printf("Saved Notes:\n");
+            table_header = 1;
+        }
+
+        // Sendfile trolololo?...
+        printf(" > %s\n", entry->d_name);
+    }
+
+    closedir(dirfd);
+}
+
+void delete_note(struct User* user) {
+    printf("<Idx> of Note to delete?\n> ");
+    
+    long note_idx = getlong();
+    if ((note_idx >= 0) && (note_idx < 10)) {
+        if (user->in_use[note_idx]) {
+            user->in_use[note_idx] = 0;
+        } else {
+            printf("Note %d doesn't exist!\n", note_idx);
+            // Maybe actually free the given val?
+        }
+    } else {
+        puts("Invalid Idx!");
+    }
+}
+
+void load_note(struct User* user) {
+    printf(
+        "Which note to load?" NL
+        "Filename > "
+    );
+    
+    char path_buf[sizeof(STORAGE_DIR) + sizeof(user->username) + 0x20];
+    int bytes_written = snprintf(path_buf, sizeof(path_buf), STORAGE_DIR, user->username);
+    fgets(path_buf + bytes_written, sizeof(path_buf) - bytes_written, stdin);
+    
+    printf(
+        "Which slot should it be stored in?" NL
+        "> "
+    );
+
+    long idx = getlong();
+    if ((idx <0) || (idx > 10)) {
+        puts("Invalid Idx!");
+        return;
+    } 
+
+    int filefd = open(path_buf, O_RDONLY);
+    if (filefd < 0) {
+        printf("Failed to open %s" NL, path_buf);
+    }
+
+    int bytes_read = read(filefd, user->notes[idx], NOTE_SIZE);  
+    if (bytes_read < 0) {
+        perror("Note read failed");
+        exit(EXIT_FAILURE);
+    }  
+    close(filefd);
+
+    user->notes[idx][bytes_read] = 0;
+    printf("Note %s was loaded into Slot %d." NL, path_buf, idx);
+}
+
+void save_note(struct User* user) {
+    printf(
+        "Which note to save?" NL
+        "> "
+    );
+    
+    long idx = getlong();
+    if ((idx <0) || (idx > 10)) {
+        puts("Invalid Idx!");
+        return;
+    }
+
+    // Check if note exists
+
+    printf(
+        "Which File to save into?" NL
+        "Filename > "
+    );
+
+    char path_buf[sizeof(STORAGE_DIR) + sizeof(user->username) + 0x20];
+    int bytes_written = snprintf(path_buf, sizeof(path_buf), STORAGE_DIR, user->username);
+    fgets(path_buf + bytes_written, sizeof(path_buf) - bytes_written, stdin);
+
+    // TODO: Sanitize path
+    long filefd = open(path_buf, O_WRONLY | O_CREAT, 0644);
+    if (filefd < 0) {
+        perror("Failed to open file!");
+        exit(EXIT_FAILURE);
+    }
+
+    if (0 > write(filefd, user->notes[idx], strlen(user->notes[idx]))) {
+        perror("Failed to write note!");
+        exit(EXIT_FAILURE);
+    }
+    close(0);
+}
+
+int main(int argc, const char * argv[]) {
+    int menu_counter = 0;
+    struct User* current_user = NULL;
+    
+    setup();
+    puts("Welcome to Bambi-Notes!");
+
+    while (1) {
+        main_menu();
+        long menu_option = getlong();
+        menu_counter++;
+
+        switch (menu_option)
+        {
+        case 1:
+            current_user = user_register();
+            break;
+        
+        case 2:
+            current_user = user_login();
+            break;
+        
+        case 1337:
+            break;
+
+        default:
+            break;
+        }
+
+        if (current_user != 0) break;
+    }
+
+    while (current_user > 0) {
+        authenticated_menu(current_user);
+        long menu_option = getlong();
+        // menu_counter++;
+
+        switch (menu_option)
+        {
+        case 1:
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
+    }
+
+    puts("Bye!");
+}
